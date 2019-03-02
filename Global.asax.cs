dc61bb0e751ef4e0cd8b999b5b7d6a181ef30ffd
lao er <35189933+laoer2009@users.noreply.github.com>
2019-03-02 20:34:48 +0800

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Abp;
+using Abp.Web;
+namespace MyTweet.Web
+{
+    public class MvcApplication :AbpWebApplication<MyTweet.Web.App_Start.MyTweetWebModule>
+    {
+        protected override void Application_Start(object sender,EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            base.Application_Start(sender, e);
+        }
+    }
+}
